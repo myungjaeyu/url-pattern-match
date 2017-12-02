243e9cfe26aede8a8bd83567af21ad32186f514d
u4bi <rootcode10@gmail.com>
2017-12-02 22:26:27 +0900

    dist file
    - url-match.js
    - url-match.min.js

---
+++ dist/url-match.js
@@ -0,0 +1,43 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.match = factory());
+}(this, (function () { 'use strict';
+
+var patternEscape = (pattern) => pattern.replace(/[\?\&\.]/g, '\\$&');
+var expression = (pattern) => new RegExp(`^${ pattern.replace(/\:[^\/\&\.\?]+/g, '([^\/]+)') }$`);
+
+var getKeys = (pattern) => {
+    try {
+        return pattern.match(/\:([^\?\/\\]+)/g).map(e => e.slice(1));
+    } catch(e) {
+        return null;
+    }
+};
+
+var getMatch = (url, keys, regex) => url.match(regex);
+
+var isMatch = (state, children = {} ) => {
+    return { state : state, children : children };
+};
+
+var match = (pattern, url) => {
+    if (pattern === url) return isMatch(true);
+    
+    pattern = patternEscape(pattern);
+
+    let keys = getKeys(pattern),
+        results = getMatch(url, keys, expression(pattern));
+
+    if(!keys || !results ) return isMatch(false);
+    
+    return isMatch(true, results.splice(1, keys.length)
+                                .reduce((obj, e, index) => {
+                                    obj[keys[index]] = e;
+                                    return obj;
+                                }, {} ));
+};
+
+return match;
+
+})));
